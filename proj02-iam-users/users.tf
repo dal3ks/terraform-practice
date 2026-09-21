ad28ef3cf6ad7e16d2a09b7fdbf68fdaa83edcf4
Alex S <220056072+dal3ks@users.noreply.github.com>
2026-09-21 16:47:25 +0100

    Create IAM users from YAML configuration

--- proj02-iam-users/users.tf
+++ proj02-iam-users/users.tf
@@ -1,7 +1,11 @@
 locals {
-    users_from_yaml = yamldecode(file("${path.module}/user-roles.yaml"))
+  users_from_yaml = yamldecode(file("${path.module}/user-roles.yaml")).users
 }
 
+resource "aws_iam_user" "users" {
+  for_each = toset(local.users_from_yaml[*].username)
+  name     = each.value
+}
 output "users" {
-    value = local.users_from_yaml
+  value = local.users_from_yaml
 }
