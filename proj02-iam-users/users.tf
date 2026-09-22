0431b808b570e5bf9d7a9fedcdda4b2e821069a0
Alex S <220056072+dal3ks@users.noreply.github.com>
2026-09-22 11:02:01 +0100

    Configure IAM user console access

--- proj02-iam-users/users.tf
+++ proj02-iam-users/users.tf
@@ -6,6 +6,24 @@ resource "aws_iam_user" "users" {
   for_each = toset(local.users_from_yaml[*].username)
   name     = each.value
 }
-output "users" {
-  value = local.users_from_yaml
+
+resource "aws_iam_user_login_profile" "users" {
+  for_each        = aws_iam_user.users
+  user            = each.value.name
+  password_length = 8
+
+
+  lifecycle {
+    ignore_changes = [
+      password_length,
+      password_reset_required,
+      pgp_key
+    ]
+  }
+}
+
+output "passwords" {
+  value = {
+  for user, user_login in aws_iam_user_login_profile.users : user => user_login.password }
+  sensitive = true
 }
